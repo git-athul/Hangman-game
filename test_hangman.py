b503634177d298a374e8cc5b592faf19bd20b039
athul <kpathul@outlook.com>
2019-02-12 20:26:18 +0530

    RED: Adds test for condition 1

--- test_hangman.py
+++ test_hangman.py
@@ -1,7 +1,7 @@
 # test_hangman.py
 
 import hangman
-
+# A. Getting secret word
 # 1. Secret word should have atleast 6 letters
 # 2. Secret word should have no punctuation
 # 3. Secret word should not be a proper noun
@@ -16,6 +16,7 @@ def test_secret_word_no_proper_nouns():
     assert all(hangman.get_secret_word("./test_data/3.words") == "policeman" for _ in range(100))
 
 
+# B. Masking secret word    
 # 1. Masks entire word when not guessed.
 # 2. Unmasks entire word when fully guessed.
 # 3. Unmasks for words with repetitive letters.
@@ -39,6 +40,16 @@ def test_mask_word_repetitive():
     assert all([ hangman.mask_word(words[i], guess[i]) == masks[i] for i in range(len(words)) ])
 
 
+# C. Checking inputs
+# 1. Condition1:  One letter guesses
+
+def test_check_cond1():
+    guess =[ "you", "we", "them", "u", "i", "v" ]
+    result=[False, False, False, True, True, True ]
+    assert ([hangman.check_cond1(guess[i]) == result[i] for i in range(len(guess)) ])
+
+
+    
 # Counting wrong guesses
 
 def test_wrong_guess():
